1c28fef6c2a867947d8b4230445e8771baeb0565
Rian18 <rianalves2009.ra@gmail.com>
2021-05-22 11:49:31 -0300

    Implementação do Pivot Randômico com duas opções.

--- Quicksort/src/pivot/PivotRandomico.java
+++ Quicksort/src/pivot/PivotRandomico.java
@@ -6,7 +6,28 @@ public class PivotRandomico implements Pivot{
 
 	@Override
 	public Integer findPivot(List<Integer> sortArray) {
-		Integer inicio, fim, pivot=0;
+		// Pegamos o primeiro e último item da lista...  
+		Integer inicio = sortArray.get(0), 
+				fim = sortArray.get(sortArray.size()),
+				pivot= (1)/(fim-inicio+1);
+
+		//ou Pegamos o menor e o maior
+		/*		
+		Integer menor = sortArray.get(0),
+				maior = sortArray.get(0),
+				
+		for (int i = 1; i < sortArray.size(); i++) {
+			if (sortArray.get(i) < menor) {
+				menor = sortArray.get(i);
+
+			}else if (sortArray.get(i)> maior) {
+				sortArray.get(sortArray.size());
+			}{
+				maior = sortArray.get(i);		
+			}
+		}
+		Integer pivot= (1)/(maior-menor+1);
+		*/
 		return pivot;
 	}
 }
