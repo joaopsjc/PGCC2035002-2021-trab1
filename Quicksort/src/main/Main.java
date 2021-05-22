b466d043b26276a987e415fd37caeae5d3c6a875
Rian18 <rianalves2009.ra@gmail.com>
2021-05-22 13:08:54 -0300

    Inserção de elementos na lista para teste

--- Quicksort/src/main/Main.java
+++ Quicksort/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import pivot.Pivot;
 import pivot.PivotFactory;
@@ -9,8 +10,14 @@ import quicksort.Quicksort;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		// Adição de elementos na lista para teste 
+		Random random = new Random();
+	
 		List<Integer> sortArray = new ArrayList<>();
+
+		for (int i = 0; i < 5; i++) {
+			sortArray.add(random.nextInt(10));
+		}
 		
 		Pivot pivotAtual = PivotFactory.create("Mediano");
 		Quicksort algoritmoSort = new Quicksort(sortArray);
