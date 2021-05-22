c167b010b56e0aa67ef433d49d3784d96c9bcc81
Rian18 <rianalves2009.ra@gmail.com>
2021-05-22 13:05:59 -0300

    Implementação Pivot Indexado

--- Quicksort/src/pivot/PivotIndexado.java
+++ Quicksort/src/pivot/PivotIndexado.java
@@ -2,11 +2,17 @@ package pivot;
 
 import java.util.List;
 
-public class PivotIndexado implements Pivot{
+public class PivotIndexado implements Pivot {
 
 	@Override
 	public Integer findPivot(List<Integer> sortArray) {
-		// TODO Auto-generated method stub
-		return 0;
+		Integer inicio = sortArray.get(0), 
+				fim = sortArray.get(sortArray.size()),
+				inteiro =  (inicio +fim)/2,
+				pivot= (inicio + fim + inteiro)/3;
+
+		return pivot;
 	}
+
+	
 }
