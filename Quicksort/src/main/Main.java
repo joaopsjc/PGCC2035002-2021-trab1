7440893e09993142f0e3f849077d756492a99a2a
Joao <Joao@DESKTOP-PMM29F5>
2021-05-23 16:55:52 -0300

    feita interface de interação

--- Quicksort/src/main/Main.java
+++ Quicksort/src/main/Main.java
@@ -1,16 +1,12 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-import pivot.Pivot;
-import pivot.PivotFactory;
-import quicksort.Quicksort;
 
 public class Main {
+	
+	
+	
 	public static void main(String[] args) {
-		// Adição de elementos na lista para teste 
+		/*
 		Random random = new Random();
 	
 		List<Integer> sortArray = new ArrayList<>();
@@ -29,6 +25,8 @@ public class Main {
 		sortArray = algoritmoSort.sortList(pivotAtual);
 		for (Integer integer : sortArray) {
 			System.out.println(integer);
-		}
+		}*/
+		Panel painel = new Panel();
+		painel.inicia();
 	}
 }
---
+++ Quicksort/src/main/Panel.java
@@ -0,0 +1,135 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import pivot.Pivot;
+import pivot.PivotFactory;
+import quicksort.Quicksort;
+
+public class Panel {
+	 private void ClearConsole(){
+	        try{
+	            String operatingSystem = System.getProperty("os.name"); //Check the current operating system
+	              
+	            if(operatingSystem.contains("Windows")){        
+	                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+	                Process startProcess = pb.inheritIO().start();
+	                startProcess.waitFor();
+	            } else {
+	                ProcessBuilder pb = new ProcessBuilder("clear");
+	                Process startProcess = pb.inheritIO().start();
+
+	                startProcess.waitFor();
+	            } 
+	        }catch(Exception e){
+	            System.out.println(e);
+	        }
+ }
+	public void inicia()
+	{
+		Pivot pivotAtual;
+		String input = null;
+		ClearConsole();
+		while(input==null || !input.equals("q"))
+		{	
+			System.out.println(
+					"Escolha qual função pivot utilizar: \n" +
+					"1:Mediano. \n" +
+					"2:AchaPivot. \n" +
+					"3:Randômico. \n" +
+					"4:Indexado. \n" +
+					"ou 'q' para sair.");
+			
+	        try {
+		        BufferedReader reader = new BufferedReader(
+			            new InputStreamReader(System.in));
+				input = reader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			ClearConsole();
+			switch(input)
+			{
+				case "1":
+					pivotAtual = PivotFactory.create("Mediano");
+					ordena(pivotAtual);
+					break;
+				case "2":
+					pivotAtual = PivotFactory.create("AchaPivot");
+					ordena(pivotAtual);
+					break;
+				case "3":
+					pivotAtual = PivotFactory.create("Randomico");
+					ordena(pivotAtual);
+					break;
+				case "4":
+					pivotAtual = PivotFactory.create("Indexado");
+					ordena(pivotAtual);
+					break;
+				case "q":
+					break;
+				default:
+					System.out.println("O valor digitado não é aceito");
+					
+			}
+
+		}
+	}
+	private void imprime(List<Integer> sortArray)
+	{
+		for (Integer integer : sortArray) {
+			System.out.print(integer + ", ");
+		}
+		System.out.println("");
+	}
+	
+	private void ordena(Pivot pivotAtual)
+	{
+		int tamanho=0;
+        BufferedReader reader = new BufferedReader(
+	            new InputStreamReader(System.in));
+		
+        try {
+        	
+			while(tamanho<1)
+			{
+				System.out.println("Qual o tamanho da cadeia a ser ordenada?");	
+			    tamanho = Integer.parseInt(reader.readLine());
+			    ClearConsole();
+			}
+			
+			Random random = new Random();
+			List<Integer> sortArray = new ArrayList<>();
+			
+			for (int i = 0; i < tamanho; i++) {
+				sortArray.add(random.nextInt(tamanho));
+			}
+			
+			System.out.print("array gerado:");
+			imprime(sortArray);
+			
+			long start = System.nanoTime();
+			
+			Quicksort algoritmoSort = new Quicksort(sortArray);
+			List<Integer> sortedArray = algoritmoSort.sortList(pivotAtual);
+			long elapsedTime = System.nanoTime() - start;
+			
+			System.out.print("array ordenado:");
+			imprime(sortedArray);
+			System.out.println("Tempo de processamento:" + elapsedTime);
+
+	        reader.read();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
